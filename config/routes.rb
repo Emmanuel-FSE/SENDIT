0c0d0fb7aaaecc59eead9e559767d557d282807f
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-04-14 14:46:35 +0300

    Create the endpoints for parcels

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :statuses
-  resources :parcels
+  resources :parcels, only: [:index, :show, :create, :update]
   resources :users, only: [:index, :show, :create, :update]
 end
