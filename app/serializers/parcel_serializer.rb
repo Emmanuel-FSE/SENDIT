a695a1d4119bbe69fa45e23637479bd199923b7d
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-04-18 11:48:50 +0300

    Add user to parcel

--- app/serializers/parcel_serializer.rb
+++ app/serializers/parcel_serializer.rb
@@ -1,4 +1,5 @@
 class ParcelSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :parcel_name, :pickup_location, :destination, :weight, :price
   has_one :status
+  belongs_to :user
 end
