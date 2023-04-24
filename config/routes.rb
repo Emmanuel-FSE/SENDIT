1652e3ec6a79f1bbd91bf55333a503edc01a6ad2
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2023-04-24 12:57:19 +0300

    Add ability to update the status

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :statuses
+  resources :statuses, only: [:index, :update]
   resources :parcels, only: [:index, :show, :create, :update]
   resources :users, only: [:index, :show, :create, :update]
   post "/login", to: "sessions#create"
